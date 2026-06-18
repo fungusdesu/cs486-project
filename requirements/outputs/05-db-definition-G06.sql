7aa2e578d941f16ac9e815fb2e6e18ad2c41b2e5
dd0d <thienlacq2801@gmail.com>
2026-06-18 13:41:10 +0700

    added basci entities

---
+++ requirements/outputs/05-db-definition-G06.sql
@@ -0,0 +1,61 @@
+SET NOEXEC ON;
+GO
+
+CREATE DATABASE School;
+GO
+
+
+CREATE TABLE [User] (
+    user_id varchar(8) PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar (7) NOT NULL,
+    full_name as CONCAT(first_name, ' ', last_name),
+    email varchar(100) NOT NULL,
+    phone_number varchar(100) NOT NULL,
+
+    role_ nvarchar(30) NOT NULL,
+    CONSTRAINT chk_user_role CHECK (role_ in ("Student", "Lecturer", "Teaching Assistant", "Facility Staff", "Department Administrator", "Facility Manager")),
+
+    department nvarchar(20) NOT NULL,
+
+    account_status nvarchar(20) NOT NULL,
+    CONSTRAINT chk_account_status CHECK (
+        account_status in ("Active", "Disabled", "Pending Approval", "Deleted")
+    ) 
+);
+
+CREATE TABLE Space (
+    space_id nvarchar(20) PRIMARY KEY,
+    space_name nvarchar(50) NOT NULL,
+
+    space_type nvarchar(20) NOT NULL,
+    CONSTRAINT chk_space_type CHECK (
+        space_type in ("Classroom", "Meeting Room", "Laboratory", "Lecture Hall", "Other")
+    )
+
+    building nvarchar(20) NOT NULL,
+    floor nvarchar(20) NOT NULL,
+    room_number nvarchar(20) NOT NULL,
+    space_location as CONCAT(building, '', floor, '', room_number),
+
+    capacity int NOT NULL,
+
+    current_status nvarchar(20) NOT NULL,
+    CONSTRAINT chk_current_status CHECK (
+        current_status in ("Available", "In Use", "Under Maintenance", "Closed", "Retired")
+    )
+
+    usage_policy nvarchar(500) NOT NULL,
+
+
+);
+
+CREATE TABLE Facility (
+    facility_id nvarchar(20) PRIMARY KEY,
+    facility_name nvarchar(50) NOT NULL,
+);
+
+
+
+SET NOEXEC OFF;
+GO
