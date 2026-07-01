7c204e3423677261dd30d88572ffad163ac66304
Quach-Thien-Lac <thienlacq2801@gmail.com>
2026-07-01 19:50:03 +0700

    added spacepolicy

---
+++ outputs/06-sample-data-G06.sql
@@ -0,0 +1,19 @@
+INSERT INTO SpacePolicy (
+    space_policy_id,
+    booking_window_days,
+    min_duration_minutes,
+    max_duration_minutes,
+    check_in_grace_minutes
+)
+VALUES
+    ('DYWGI', 7, 10, 300, 5),
+    ('HKBSL', 3, 10, 120, 10),
+    ('NCYTN', 7, 10, 300, 10),
+    ('QFJJO', 2, 10, 180, 5),
+    ('CDQNA', 3, 10, 250, 5),
+    ('QRSCP', 7, 10, 150, 3),
+    ('FXADX', 3, 10, 360, 3),
+    ('LRYBE', 3, 10, 360, 5),
+    ('CXWSK', 3, 10, 300, 10),
+    ('DTEHH', 3, 10, 120, 10);
+GO
