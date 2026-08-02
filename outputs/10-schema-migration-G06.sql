632beb7c6c0b443c05bec7e150a6977de8fee984
Quach-Thien-Lac <thienlacq2801@gmail.com>
2026-08-02 18:09:32 +0700

    fucking typo fml

--- outputs/10-schema-migration-G06.sql
+++ outputs/10-schema-migration-G06.sql
@@ -109,8 +109,10 @@ BEGIN TRY
     ADD maintenance_time_slot DATETIME NOT NULL;
 
     INSERT INTO Maintenance (maintenance_id, maintenance_time_slot)
-    SELECT DATEDIFF(MINUTE, maintenance_start_time, maintenance_end_time) AS maintenance_time_slot
+    SELECT maintenance_id, DATEDIFF(MINUTE, maintenance_start_time, maintenance_end_time) AS maintenance_time_slot
     FROM junction_table.Maintaining;
+
+
 
 
     COMMIT TRANSACTION;
