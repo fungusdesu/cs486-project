33dba8b4deec9f0de99789660a0d9cfcc53b3c39
Quach-Thien-Lac <thienlacq2801@gmail.com>
2026-08-08 18:34:49 +0700

    small fixes on first function

--- outputs/12-f-concurrency-implementation-function.sql
+++ outputs/12-f-concurrency-implementation-function.sql
@@ -10,17 +10,19 @@ CREATE OR ALTER FUNCTION GetAllAdvisories
 RETURNS TABLE 
 AS
 RETURN (
-    SELECT m.maintenance_id, m.maintenance_name, ms.maintenance_session_id, ms.maintenance_impact_level_id
+    SELECT m.maintenance_id, m.maintenance_description, mts.maintenance_status_name
     FROM Maintenance m  
     JOIN MaintenanceSession ms ON m.maintenance_id = ms.maintenance_id
+    JOIN lookup_table.MaintenanceStatus mts ON m.maintenance_status_id = mts.maintenance_status_id
     WHERE ms.maintenance_impact_level_id IN (
         SELECT maintenance_impact_level_id 
-        FROM MaintenanceImpactLevel 
+        FROM lookup_table.MaintenanceImpactLevel 
         WHERE maintenance_impact_level_name = 'Advisory'
     )
 );
 
 GO  
+
 
 
     
